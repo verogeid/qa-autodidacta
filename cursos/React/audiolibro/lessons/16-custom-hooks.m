97b7a46f58110eecb3dd8aa43550e542e4b8f0e8
Diego González Fernández <162438292+verogeid@users.noreply.github.com>
2025-05-17 03:16:04 +0200

    Update and rename cursos/React/audiolibro/semana16.md to cursos/React/audiolibro/lessons/16-custom-hooks.m

--- cursos/React/audiolibro/lessons/16-custom-hooks.m
+++ cursos/React/audiolibro/lessons/16-custom-hooks.m
@@ -1,4 +1,4 @@
-Semana 16: Custom Hooks, reutilizando lógica con estilo
+16: Custom Hooks, reutilizando lógica con estilo
 
 Cuando tienes trozos de lógica que se repiten en varios componentes, escribirla una y otra vez es un error.
 
